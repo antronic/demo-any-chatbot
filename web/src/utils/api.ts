09d90abcadeefd6229184b46751c07d060624d5f
Jirachai Chansivanon <jirachai.c@outlook.com>
2024-04-23 06:14:19 +0700

    🛠 Fix wrong Vite Env var prefix

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,4 +1,4 @@
-const HOST = import.meta.env.API_URL || 'http://127.0.0.1:5000'
+const HOST = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000'
 
 // Api hooks
 export const sendMessage = async (message: string, history: { chat: string, role: string }[] | string) => {
