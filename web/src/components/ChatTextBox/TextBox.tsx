8b2ca80d66a11a68ba6631935efc02fd5d89c298
Jirachai Chansivanon <antronic@users.noreply.github.com>
2024-04-22 21:17:45 +0000

    Refactor TextBox component to remove customize color functionality

--- web/src/components/ChatTextBox/TextBox.tsx
+++ web/src/components/ChatTextBox/TextBox.tsx
@@ -44,35 +44,6 @@ export const TextBox: React.FC<ITextBoxProps> = () => {
     setIsTyping(false)
   }
 
-  // List 7 rainbow colors button to set the button color
-  function handleColorChange(color: string) {
-    setThemeColor(color)
-  }
-
-  // Render 7 rainbow colors button to set the button color with name of the color
-  function listButtons() {
-    const colors = [
-      'bg-red-400',
-      'bg-orange-400',
-      'bg-yellow-400',
-      'bg-green-400',
-      'bg-blue-400',
-      'bg-indigo-400',
-      'bg-purple-400',
-    ]
-
-    return colors.map((color) => (
-      <button
-        key={color}
-        className={`${color} text-white p-2 border border-white`}
-        onClick={() => handleColorChange(color)}
-      >
-        {color.split('-')[1]}
-      </button>
-    ))
-
-  }
-
   return (
     <div>
       <div className="flex items-center justify-between w-full gap-x-2">
@@ -100,11 +71,6 @@ export const TextBox: React.FC<ITextBoxProps> = () => {
 
       <div className="text-sm my-2">
         <p>{isTyping ? 'Bot is typing...' : ''}</p>
-      </div>
-
-      {/* Customize your colors */}
-      <div className="flex gap-x-2">
-        {listButtons()}
       </div>
     </div>
   )
